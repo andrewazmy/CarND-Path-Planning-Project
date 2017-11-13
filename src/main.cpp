cdf76ffd064eec0006beba40b3adc3aa46bae0f6
andrewazmy <andrewkamal@gmail.com>
2017-11-13 19:37:35 +0200

    speed control depending on same lane car

--- src/main.cpp
+++ src/main.cpp
@@ -199,7 +199,7 @@ int main() {
   int lane = 1;
   double ref_vel = 49.5; //mph
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  h.onMessage([&ref_vel,&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy,&lane](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -240,7 +240,47 @@ int main() {
           	json msgJson;
 
           	vector<double> next_x_vals;
-          	vector<double> next_y_vals;
+			vector<double> next_y_vals;
+			  
+
+			//TODO: check for collision using sensor fusion
+			if (prev_size > 0)
+			{
+				car_s = end_path_s;
+			}
+			bool too_close = false;
+
+			for (int i = 0; i < sensor_fusion.size(); i++)
+			{
+				float d = sensor_fusion[i][6];
+
+				if (d < (2 + 4*lane + 2) && d > (2 + 4*lane - 2))
+				{
+					//car is in my lane
+					double vx = sensor_fusion[i][3];
+					double vy = sensor_fusion[i][4];
+					double check_car_s = sensor_fusion[i][5];
+					double check_speed = sqrt(vx*vx + vy*vy);
+
+					check_car_s += (double)prev_size*0.02*check_speed;
+
+					if (check_car_s > car_s && (check_car_s - car_s) < 30)
+					{
+						// ref_vel = check_speed;
+						// lane += 1;
+						too_close = true;
+					}
+				}
+			}
+
+			if (too_close)
+			{
+				ref_vel -= 0.224;
+			}
+			else if (ref_vel < 49.5)
+			{
+				ref_vel += 0.224;
+			}
 
 
 			// TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
@@ -253,7 +293,8 @@ int main() {
 			double ref_y = car_y;
 			double ref_yaw = deg2rad(car_yaw);
 
-			if (prev_size < 2){
+			if (prev_size < 2)
+			{
 				double prev_car_x = car_x - cos(car_yaw);
 				double prev_car_y = car_y - sin(car_yaw);
 
@@ -263,8 +304,8 @@ int main() {
 				ptsy.push_back(prev_car_y);
 				ptsy.push_back(car_y);
 			}
-			else{
-
+			else
+			{
 				ref_x = previous_path_x[prev_size-1];
 				ref_y = previous_path_y[prev_size-1];				
 				
@@ -291,18 +332,21 @@ int main() {
 			ptsy.push_back(next_wp_0[1]);
 			ptsy.push_back(next_wp_1[1]);
 			ptsy.push_back(next_wp_2[1]);
-			for (int i = 0; i < ptsx.size(); i++){
+
+			for (int i = 0; i < ptsx.size(); i++)
+			{
 				double shift_x = ptsx[i] - ref_x;
 				double shift_y = ptsy[i] - ref_y;
 
 				ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
-				ptsx[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+				ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
 			}
 
 			tk::spline s;
 			s.set_points(ptsx, ptsy);
 
-			for (int i = 0; i < previous_path_x.size(); i++){
+			for (int i = 0; i < previous_path_x.size(); i++)
+			{
 				next_x_vals.push_back(previous_path_x[i]);
 				next_y_vals.push_back(previous_path_y[i]);
 			}
@@ -313,7 +357,28 @@ int main() {
 
 			double x_add_on = 0;
 
-			
+			for (int i = 0; i <= 50 - previous_path_x.size(); i++)
+			{
+				double N = target_dist/(0.02*ref_vel/2.24);
+				double x_point = x_add_on + target_x/N;
+				double y_point = s(x_point);
+
+				x_add_on = x_point;
+				
+				double x_ref = x_point;
+				double y_ref = y_point;
+
+				x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+				y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+				x_point += ref_x;
+				y_point += ref_y;
+
+				next_x_vals.push_back(x_point);
+				next_y_vals.push_back(y_point);
+			}
+
+
 				
 
 			
